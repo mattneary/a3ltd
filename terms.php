e76094bf7a99b404aa61e42ea46e6a0207df3009
Matt Neary <neary.matt@gmail.com>
2015-09-06 20:51:39 -0400

    extract terms as separate file

--- terms.php
+++ terms.php
@@ -10,9 +10,7 @@ include "header.php";
 <div id="main" class="container">
 	<h2>Terms of Use</h2>
 	<br>
-	<p>Our legal guys are working very hard on this! Come back soon for complete details</p>
-	<br>
-	<img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Frustrated_man_at_a_desk_%28cropped%29.jpg" style="width: 60%; min-width: 240px;">
+  <? include "termscontents.php"; ?>
 </div>	
 <?php 
 include "footer.php"
---
+++ termscontents.php
@@ -0,0 +1,2 @@
+<p>Our legal guys are working very hard on this! Come back soon for complete details</p>
+<br>
