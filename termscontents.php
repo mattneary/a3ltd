6062c03f3f345423645753573a366944fd1c887a
Matt Neary <neary.matt@gmail.com>
2015-09-06 20:57:08 -0400

    include offical terms

--- termscontents.php
+++ termscontents.php
@@ -1,2 +1,85 @@
-<p>Our legal guys are working very hard on this! Come back soon for complete details</p>
-<br>
+<pre>
+By subscribing to A3 Limited you are agreeing to the terms and conditions.
+
+By agreeing to the terms and conditions you are agreeing that any work you complete for A3 Limited will be completed as independent contractor and that A3 Limited will not be your primary means of income.
+
+The Parties therefore agree as follows:
+
+1.0.Term and Termination.
+
+1.1. This Agreement takes effect immediately as of the Effective Date, and remains in full force and effect until the Contractor has completed the Services (the "Term"), unless earlier terminated under this Section 1.
+
+1.2. Either Party may terminate this Agreement for cause by providing the other Party written notice if the other Party: (i) is in material breach of this Agreement and has failed to cure such breach within five (5) days after its receipt of written notice of such breach provided by the non-breaching Party; (ii) engages in any unlawful business practice related to that Party's performance under the Agreement; or (iii) files a petition for bankruptcy, becomes insolvent, acknowledges its insolvency in any manner, ceases to do business, makes an assignment for the benefit of its creditors, or has a receiver, trustee or similar party appointed for its property.
+
+2.0.Contractor Services.
+
+2.1. During the Term, the Company may engage the Contractor to provide the following services as needed (the "Services"), or other such services as mutually agreed upon in writing by the Parties (email is acceptable):
+
+2.2. The Contractor shall provide the necessary equipment to perform the Services. If the Contractor has obtained employees or agents (the "Contractor Personnel"), the Contractor shall be solely responsible for all costs associated with the Contractor Personnel.
+
+ 
+
+2.3 As a result of providing the Services, the Contractor or Contractor Personnel may create certain work product (the "Work Product").
+
+
+
+2.4The Company shall not be responsible for federal, state and local taxes derived from the Contractor's net income or for the withholding and/or payment of any federal, state and local income and other payroll taxes, workers' compensation, disability benefits or other legal requirements applicable to the Contractor.
+
+3.0.Independent Contractor Status.
+
+3.1. The Parties intend that the Contractor and any Contractor Personnel be engaged as independent contractors of Company. Nothing contained in this Agreement will be construed to create the relationship of employer and employee, principal and agent, partnership or joint venture, or any other fiduciary relationship.
+
+3.2. The Contractor may not act as agent for, or on behalf of, the Company, or to represent the Company, or bind the Company in any manner.
+
+3.3. The Contractor will not be entitled to worker's compensation, retirement, insurance or other benefits afforded to employees of the Company.
+
+4.0.Ownership.
+
+4.1. The Parties intend that, to the extent the Work Product or a portion of the Work Product qualifies as a "work made for hire," within the definition of Section 101 of the Copyright Act of the United States (17 U.S.C. § 101), it will be so deemed a work made for hire. If the Work Product or any portion of the Work Product does not qualify as work made for hire, and/or as otherwise necessary to ensure the Company's complete ownership of all rights, titles and interest in the Work Product, the Contractor shall transfer and assign to the Company all rights, titles and interests throughout the world in and to any and all Work Product. This transfer and assignment includes, but is not limited to, the right to publish, distribute, make derivative works of, edit, alter or otherwise use the Work Product in any way the Company sees fit.
+
+4.2. The Company grants the Contractor, a limited, non-exclusive, non-transferable, non-assignable, royalty free, worldwide license to display the Work Product on a platform personally controlled, in whole or in part, by the Contractor. The Company may revoke this license at any time by requesting the removal of the Work Product displayed by the Contractor. Upon such request, the Contractor shall remove the Work Product from the platform, and provide written notification of such removal.
+
+5.0. Representations. Both Parties represent that they are fully authorized and empowered to enter into this Agreement, and that the performance of the obligations under this Agreement will not violate or infringe upon the rights of any third-party, or violate any agreement between the Parties and any other person, firm or organization or any law or governmental regulation.
+
+6.0. Indemnification. The Contractor shall indemnify and hold harmless the Company, its affiliates, and its respective officers, directors, agents and employees from any and all claims, demands, losses, causes of action, damage, lawsuits, judgments, including attorneys’ fees and costs, arising out of, or relating to, the Contractor’s services under this Agreement.
+
+7.0.Confidential Information.
+
+7.1 Each Party (on its behalf and on behalf of its subcontractors, employees or representatives, or agents of any kind) agrees to hold and treat all confidential information of the other Party, including, but not limited to, trade secrets, sales figures, employee and customer information and any other information that the receiving Party reasonably should know is confidential (“Confidential Information”) as confidential and protect the Confidential Information with the same degree of care as each Party uses to protect its own Confidential Information of like nature.
+
+7.2 Confidential Information does not include any information that (i) at the time of the disclosure or thereafter is lawfully obtained from publically available sources generally known by the public (other than as a result of a disclosure by the receiving Party or its representatives); (ii) is available to the receiving Party on a non-confidential basis from a source that is not and was not bound by a confidentiality agreement with respect to the Confidential Information; or (iii) has been independently acquired or developed by the receiving Party without violating its obligations under this Agreement or under any federal or state law.
+
+8.0. Liability. EXCEPT WITH RESPECT TO THE PARTIES’ INDEMNIFICATION OBLIGATIONS, NEITHER PARTY SHALL BE LIABLE TO THE OTHER FOR ANY SPECIAL, INDIRECT, INCIDENTAL, PUNITIVE, OR CONSEQUENTIAL DAMAGES ARISING FROM OR RELATED TO THIS AGREEMENT, INCLUDING BODILY INJURY, DEATH, LOSS OF REVENUE, OR PROFITS OR OTHER BENEFITS, AND CLAIMS BY ANY THIRD PARTY, EVEN IF THE PARTIES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. THE FOREGOING LIMITATION APPLIES TO ALL CAUSES OF ACTION IN THE AGGREGATE, INCLUDING WITHOOUT LIMITATION TO BREACH OF CONTRACT, BREACH OF WARRANTY, NEGLIGENCE, STRICT LIABILITY, AND OTHER TORTS.
+
+9.0. Disclaimer of Warranty. THE WARRANTIES CONTAINED HEREIN ARE THE ONLY WARRANTIES MADE BY THE PARTIES HEREUNDER. EACH PARTY MAKES NO OTHER WARRANTY, WHETHER EXPRESS OR IMPLIED, AND EXPRESSLY EXCLUDES AND DISCLAIMS ALL OTHER WARRANTIES AND REPRESENTATIONS OF ANY KIND, INCLUDING ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND NON-INFRINGEMENT. THE COMPANY DOES NOT PROVIDE ANY WARRANTY THAT OPERATION OF ANY SERVICES HEREUNDER WILL BE UNINTERRUPTED OR ERROR-FREE.
+
+10.0 Miscellaneous Provisions.
+
+10.1. This Agreement, and any accompanying appendices, duplicates, or copies, constitutes the entire agreement between the Parties with respect to the subject matter of this Agreement, and supersedes all prior negotiations, agreements, representations, and understandings of any kind, whether written or oral, between the Parties, preceding the date of this Agreement.
+
+10.2. This Agreement may be amended only by written agreement duly executed by an authorized representative of each party (email is acceptable).
+
+10.3. If any provision or provisions of this Agreement shall be held unenforceable for any reason, then such provision shall be modified to reflect the parties’ intention. All remaining provisions of this Agreement shall remain in full force and effect for the duration of this Agreement.
+
+10.4. This Agreement shall not be assigned by either party without the express consent of the other party.
+
+10.5. A failure or delay in exercising any right, power or privilege in respect of this Agreement will not be presumed to operate as a waiver, and a single or partial exercise of any right, power or privilege will not be presumed to preclude any subsequent or further exercise, of that right, power or privilege or the exercise of any other right, power or privilege.
+
+10.6. This Agreement is be governed by and construed in accordance with the laws of the State of ­­­­­­­­­­­­[JURISDICTION STATE] without reference to any principles of conflicts of laws, which might cause the application of the laws of another state. Any action instituted by either party arising out of this Agreement will only be brought, tried and resolved in the applicable federal or state courts having jurisdiction in the State of ­[JURISDICTION STATE]. EACH PARTY HEREBY CONSENTS TO THE EXCLUSIVE PERSONAL JURISDICTION AND VENUE OF THE COURTS, STATE AND FEDERAL, HAVING JURISDICTION IN THE STATE OF ­­­­­­­­­­­[JURISDICTION STATE].
+
+By agreeing to  the independent contractor is agreeing that the Company is not responsible for any health care benefits. The independent contractor is agreeing that they have other forms of income and that the Company is not their primary source of income. 
+
+
+
+By subscribing these independent contractors are agreeing that A3 Limited is not responsible for any losses or damages they may face as the result of completing work as an A3 Limited independent contractor.
+
+
+
+By subscribing to A3 Limited to have a service completed this individual is agreeing to be a client of A3 Limited who will be connected to one of their independent contractors. The client is agreeing that A3 Limited is in no way responsible for the quality of work or any issues that may arise between the client and the independent contractor.
+
+Any individual who subscribes to A3 Limited to have a service completed is agreeing to pay the monthly subscription and agreed upon price to A3 Limited in exchange for being connected to an A3 Limited contractor who will be asked to complete the agreed upon task. 
+
+ 
+
+By subscribing to A3 Limited contractors and clients are agreeing that A3 Limited is not responsible for any losses or damages they may face as the result of the work of an A3 Limited independent contractor.
+</pre>
